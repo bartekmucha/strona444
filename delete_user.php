7cd985836fe353a2ce21d7f07c9bde6d01e3db53
bartekmucha <bartekmuszynski32@gmail.com>
2025-10-23 09:27:27 +0200

    delete_user.php

---
+++ delete_user.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'config.php';
+require_once 'session_handler.php';
+
+$handler = new MySQLSessionHandler($pdo);
+session_set_save_handler($handler, true);
+session_start();
+
+if (!isset($_SESSION['loggedin'])) {
+    header("Location: login.php");
+    exit;
+}
+
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare("DELETE FROM users WHERE id = ?");
+    $stmt->execute([$_GET['id']]);
+}
+
+header("Location: index.php");
+exit;
+?>
